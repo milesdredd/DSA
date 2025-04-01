0620b5af0129d442278448ce4c5b0b977f87e492
Ankit Raj <ankitraj97093@gmail.com>
2025-04-01 19:43:11 +0530

    Added arpan.cpp on 2025-04-01

---
+++ cpp/arpan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    int arr[8] = {1,2,3,4,5,3,33,442};
+    cout<< length(arr);
+
+
+
+
+    return 0;
+}
