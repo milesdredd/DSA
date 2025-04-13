d0aba00437337d925b609d06683471a6123e9845
Ankit Raj <ankitraj97093@gmail.com>
2025-04-13 18:18:58 +0530

    Added arr.cpp on 2025-04-13

---
+++ cpp/arr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+	
+	int arr[5]={1,2,3,4,5};
+	cout << arr[3];
+	cout << endl;
+	cout << 3[arr]<< endl; 
+	cout<<"helloe sumit \n";
+
+
+	cout<< endl;
+	return 0;
+}
