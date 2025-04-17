b200708c54bee8c9ceca67ca74a69bde70982a17
Ankit Raj <ankitraj97093@gmail.com>
2025-04-17 23:29:34 +0530

    Added book.cpp on 2025-04-17

---
+++ cpp/book.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int book(int arr[], int n;int m){
+	int s =0; 
+	int sum = o;
+	for (int i = 0;i <n ; i ++ ) {
+		sum +=arr[i];
+	}
+	int e = sum;
+	cout<< sum << " pages Total ! "<< endl;
+	while (s<e){
+		if (possible(arr , n,mid)){
+			 
