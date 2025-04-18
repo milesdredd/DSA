7fa13738fb42fea2bea6c9d0a2a83c0e79c42a4d
Ankit Raj <ankitraj97093@gmail.com>
2025-04-18 20:53:41 +0530

    Added devide.cpp on 2025-04-18

---
+++ cpp/devide.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	float a;cout<< "To devide : ";cin >> a;	cout << endl;
+	float b;cout<< "Devide by : ";cin >>b;	cout << endl;
+
+	float ans = a/b;
+	cout<< "Answer is : "<< ans;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+return 0;
+
+}
+
+
+
