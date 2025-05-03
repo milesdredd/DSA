c17d153f8e7be943f030bb5848ec6826f2930de8
Ankit Raj <ankitraj97093@gmail.com>
2025-05-03 21:54:34 +0530

    Added array_overfolw_data.cpp on 2025-05-03

---
+++ cpp/array_overfolw_data.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int arr[4]= {1,2,3,4};
+   int extra  = 2;
+cout <<endl << endl<<  &arr[1]<< endl;
+cout <<endl << endl<<  &arr[2]<< endl;
+cout <<endl << endl<<  &arr[3]<< endl;
+cout <<endl << endl<<  &arr[4]<< endl;
+
+cout <<endl << endl<<  &extra<< endl;
+
+return 0;
+}
