a8b71cba7a1ee5e01b9c74f256b7aae2acb46317
Ankit Raj <ankitraj97093@gmail.com>
2025-05-08 23:59:41 +0530

    Added assignment.cpp on 2025-05-08

---
+++ cpp/assignment.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int m  =3 ; 
+   int n = 4;
+   n--= m++;
+   cout << " m is : "<< m << " \n and n is : " << n<< endl << endl;
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
