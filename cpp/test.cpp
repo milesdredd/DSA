1345cf39df77e7935ec6bde1f911031467921344
Ankit Raj <ankitraj97093@gmail.com>
2025-05-13 18:37:23 +0530

    Added test.cpp on 2025-05-13

---
+++ cpp/test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main () {
+
+	int a = 0 ;
+	for (int i =0 ; i <  3; i++){
+
+		a ++ ; 
+		cout<< a<<endl;
+	}
+	cout << "after the loop a is " << a << endl ; 
+return 0;
+}
