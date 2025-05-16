3b5595ef909688e7330dbe0c987af5310a16b414
Ankit Raj <ankitraj97093@gmail.com>
2025-05-16 20:15:12 +0530

    Added module_fuck.cpp on 2025-05-16

---
+++ cpp/module_fuck.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   float x = -15;
+   float b = 0;
+   float r = x % b;
+   cout <<"it's : " << r << endl;
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
