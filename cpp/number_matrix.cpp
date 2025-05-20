cd0090b51eb507565733cf60a5754bb1bd026d13
Ankit Raj <ankitraj97093@gmail.com>
2025-05-20 18:46:03 +0530

    Added number_matrix.cpp on 2025-05-20

---
+++ cpp/number_matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for ( int i = 0; i < (2*n-1);i++){
+        for(int j= 0 ; j < (2*n-1);j++){
+            int top = i;
+            int left = j ;
+            int right = (2*n-2)-j;
+            int bottom = (2*n-2)-i;
+            cout << (n - min(min(top,bottom),min(right,left)))<< " ";
+
+        }
+    cout<< endl;
+    }
+
+
+
+return 0;
+}
