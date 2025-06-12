e2a78873dea4f1df2f3217ef3ed32740b8078490
Ankit Raj <ankitraj97093@gmail.com>
2025-06-12 18:53:37 +0530

    Added vector.cpp on 2025-06-12

---
+++ cpp/vector.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+    cout<< "st..... \n";
+
+    vector<int> v;
+
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
+
+    for (auto raj:v){
+        cout<< raj << " ";
+    }
+
+
+
+
+
+
+
+return 0;
+}
